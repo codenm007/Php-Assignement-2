244c270e70497eb9ea692e03c2c9ba88d0450e6b
codenm007 <n.majumder14@gmail.com>
2022-04-01 23:56:09 +0530

    message improvements

--- question_1/logic.php
+++ question_1/logic.php
@@ -2,6 +2,7 @@
 
     if(isset($_REQUEST['login'])){
         if($username == "nilanjan" && $password == "password"){
+            echo "<script>alert('“Logged in successfully!')</script>";
             $_SESSION['username'] = $username;
         }else{
             echo "<script>alert('Wrong username or password !')</script>";
